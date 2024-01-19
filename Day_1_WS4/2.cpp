66b129eb1e4594e3afa257cddd432f3dd9afc81f
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-01-19 23:53:06 +0300

    Create 2.cpp

---
+++ Day_1_WS4/2.cpp
@@ -0,0 +1,41 @@
+Write a C++ program to print the following - using while loop 
+a) All natural numbers from 1 to n.
+// Write a C++ program to print the following - using while loop
+// a) All natural numbers from 1 to n.
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout << "Enter n :";
+    cin>>n;
+    for (int i=0 ; i < n ; i++){
+        cout << i<<endl;
+    }
+    return 0;
+}
+//All natural numbers in reverse (from n to 1).
+#include <iostream>
+using namespace std;
+int main (){
+    
+    int n;
+    cout << "Enter n :";
+    cin>>n;
+    for (int i=n ; i >= 0; i--){
+        cout << i<<endl;
+    }
+     
+    return 0;
+}
+//c) All alphabets from a to z
+
+#include <iostream>
+using namespace std;
+int main (){
+    char i;
+    for (char i = 'a' ; i <= 'z';i++)
+    cout << i<<endl;
+    
+    
+    return 0;
+}
