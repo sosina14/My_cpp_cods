dad1114871cf92dd450df04806ca17398036d689
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-01-20 00:09:39 +0300

    Create 3.cpp

---
+++ Day_1_WS4/3.cpp
@@ -0,0 +1,11 @@
+// 3. Write a program that displays N numbers, where N is a positive number to be entered from 
+//the keyboard. Use proper mechanisms of validating appropriate input entry.
+//a. Starting from zero.
+
+
+
+
+
+
+//b. Starting from one.
+//c. Starting from a number entered from the keyboard. 
