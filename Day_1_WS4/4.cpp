aa6b9c521b1613fd024593b2f9d569dd0035a1cf
Sosina Ayele <148296605+sosina14@users.noreply.github.com>
2024-01-21 00:33:37 +0300

    Create 4.cpp

---
+++ Day_1_WS4/4.cpp
@@ -0,0 +1,5 @@
+/ * 4. Write a program to perform the following required tasks:
+a. Check if a number is Armstrong or not. And also print all Armstrong numbers between 1 to n.
+b. Check whether a number is Perfect number or not. And also print all Perfect numbers between 1 to n.
+c. Check whether a number is Strong number or not. And also print all Strong numbers between 1 to n. */
+  
